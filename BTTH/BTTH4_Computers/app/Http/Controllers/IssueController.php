addff65d73d6d6224479f68be2f6c7b609d438a9
DangNam-IT <dangphuongnam102004@gmail.com>
2024-12-16 14:44:00 +0700

    create Controller Issue

---
+++ BTTH/BTTH4_Computers/app/Http/Controllers/IssueController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Issue;
+use Illuminate\Http\Request;
+
+class IssueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $issues = Issue::with('computers')->get();
+        return view('issues.index', compact('issues'));
+
+        // hiển thị theo kiểu phân trang
+        /*
+            public function index()
+            {
+                $issues = Issue::with('computers')->paginate(5); // lấy 5 bản ghighi
+                return view('issues.index', compact('issues'));
+            }
+        */
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        $issues = Issue::all();
+        return view('issues.create', compact('computers'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate(
+            [
+                'computer_name' => 'required|max:50',
+                'model' =>'required|max:100',
+                'operating_system' => 'required|max:50',
+                'processor'=> 'required|max:50',
+                'memory' => 'required',
+                'available'=> 'required'
+            ]
+        );
+        Issue::create($request->all());
+        return redirect()->route('issues.index')->with('success','Vấn đề đã được thêm thành công');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+        $issues = Issue::findOrFail($id);
+        return view('issues.edit', compact('issues', 'computers'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $request->validate([
+            'computer_id' => 'required',
+            'reported_by' => 'required',
+            'reported_date' => 'nullable|datetime',
+            'description' => 'required',
+            'urgency' => 'required',
+            'status' => 'required',
+        ]);
+        $issues = Issue::find($id);
+        $issues ->update($request->all());
+        
+        return redirect()->route('issues.index')->with('success','Vấn đề đã cập nhật thành công');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $issues = Issue::findOrFail($id);
+        $issues -> delete();
+        return redirect()->route('issues.index')->with('success','Đã xóa vấn đề này');
+    }
+}
